da4098e633ec436da29baf92c6ff71c0d94bcc47
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-11-01 23:09:45 +0100

    Update productoAction.js

--- src/actions/productoAction.js
+++ src/actions/productoAction.js
@@ -1,7 +1,19 @@
 import {
     CREAR_PRODUCTO,
     CREAR_PRODUCTO_EXITO,
-    CREAR_PRODUCTO_ERROR
+    CREAR_PRODUCTO_ERROR,
+    DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR,
+    ELIMINAR_PRODUCTO,
+    ELIMINAR_PRODUCTO_EXITO,
+    ELIMINAR_PRODUCTO_ERROR,
+    OBTENER_PRODUCTO,
+    OBTENER_PRODUCTO_EXITO,
+    OBTENER_PRODUCTO_ERROR,
+    EDITAR_PRODUCTO,
+    EDITAR_PRODUCTO_EXITO,
+    EDITAR_PRODUCTO_ERROR
 } from '../types';
 
 import clienteAxios from '../config/axios';
@@ -15,8 +27,7 @@ export function crearProductoAction (producto) {
             })
             .catch(() => {
                 dispatch(crearProductoError());
-            })
-        
+            })    
     }
 }
 
@@ -31,4 +42,108 @@ export const crearProductoExito = producto => ({
 
 export const crearProductoError = () => ({
     type: CREAR_PRODUCTO_ERROR
+})
+
+export function descargaProductosAction (productos) {
+    return dispatch => {
+        dispatch(descargaProductos());
+        clienteAxios.get('/productos')
+            .then(respuesta => {
+                dispatch(descargaProductosExito(respuesta.data));
+            })
+            .catch(() => {
+                dispatch(descargaProductosError());
+            })  
+    }
+}
+
+export const descargaProductos = () => ({
+    type: DESCARGA_PRODUCTOS
+})
+
+export const descargaProductosExito = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+})
+
+export const descargaProductosError = () => ({
+    type: DESCARGA_PRODUCTOS_ERROR
+})
+
+export function eliminarProductoAction (id) {
+    return dispatch => {
+        dispatch(eliminarProducto());
+        clienteAxios.delete(`/productos/${id}`)
+            .then(respuesta => {
+                dispatch(eliminarProductoExito(id));
+            })
+            .catch(() => {
+                dispatch(eliminarProductoError());
+            })  
+    }
+}
+
+export const eliminarProducto = () => ({
+    type: ELIMINAR_PRODUCTO
+})
+
+export const eliminarProductoExito = id => ({
+    type: ELIMINAR_PRODUCTO_EXITO,
+    payload: id
+})
+
+export const eliminarProductoError = () => ({
+    type: ELIMINAR_PRODUCTO_ERROR
+})
+
+export function obtenerProductoAction (id) {
+    return dispatch => {
+        dispatch(obtenerProducto());
+        clienteAxios.get(`/productos/${id}`)
+            .then(respuesta => {
+                dispatch(obtenerProductoExito(respuesta.data));
+            })
+            .catch(() => {
+                dispatch(obtenerProductoError());
+            })  
+    }
+}
+
+export const obtenerProducto = () => ({
+    type: OBTENER_PRODUCTO
+})
+
+export const obtenerProductoExito = producto => ({
+    type: OBTENER_PRODUCTO_EXITO,
+    payload: producto
+})
+
+export const obtenerProductoError = () => ({
+    type: OBTENER_PRODUCTO_ERROR
+})
+
+export function editarProductoAction (producto) {
+    return (dispatch) => {
+        dispatch(editarProducto());
+        clienteAxios.put(`/productos/${producto.id}`, producto)
+            .then(() => {
+                dispatch(editarProductoExito(producto));
+            })
+            .catch(() => {
+                dispatch(editarProductoError());
+            })    
+    }
+}
+
+export const editarProducto = () => ({
+    type: EDITAR_PRODUCTO
+})
+
+export const editarProductoExito = producto => ({
+    type: EDITAR_PRODUCTO_EXITO,
+    payload: producto
+})
+
+export const editarProductoError = () => ({
+    type: EDITAR_PRODUCTO_ERROR
 })
