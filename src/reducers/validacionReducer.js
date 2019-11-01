2168c7ed88f1209f89e837662bad39b35ef9bd0a
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-11-01 11:59:28 +0100

    Create validacionReducer.js

---
+++ src/reducers/validacionReducer.js
@@ -0,0 +1,30 @@
+import {
+    VALIDAR_FORMULARIO,
+    VALIDAR_FORMULARIO_EXITO,
+    VALIDAR_FORMULARIO_ERROR
+} from '../types';
+
+const initialState = {
+    error: false
+}
+
+export default function(state = initialState, action) {
+    switch(action.type){
+        case VALIDAR_FORMULARIO:
+            return {
+                ...state
+            }
+        case VALIDAR_FORMULARIO_EXITO:
+            return {
+                ...state,
+                error : false
+            }
+        case VALIDAR_FORMULARIO_ERROR:
+            return {
+                ...state,
+                error : true
+            }
+        default:
+            return state;
+    }
+}
