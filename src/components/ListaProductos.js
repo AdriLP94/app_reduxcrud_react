fa4413847b7bea51c822275657e986e89fd1dbe0
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-11-01 23:09:53 +0100

    Update ListaProductos.js

--- src/components/ListaProductos.js
+++ src/components/ListaProductos.js
@@ -1,11 +1,26 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
+
+import {useDispatch, useSelector} from 'react-redux';
+import {descargaProductosAction} from '../actions/productoAction'
+
+import Producto from './Producto';
 
 const ListaProductos = () => {
+
+    const dispatch = useDispatch();
+
+    const loading = useSelector(state => state.producto.loading);
+    const error = useSelector(state => state.producto.error);
+    const productos = useSelector(state => state.producto.productos);
+
+    useEffect (() => {
+        setTimeout(() => dispatch(descargaProductosAction()), 100);
+    },[dispatch])
+
     return (
     <Fragment>
         <h2 className="text-center my-5">Listado de Productos</h2>
         <table className="table table-striped">
-            
             <thead className="bg-primary table-dark">
                 <tr>
                     <th scope="col">Nombre</th>
@@ -15,11 +30,16 @@ const ListaProductos = () => {
             </thead>
 
             <tbody>
-
+                {productos.map (producto =>(
+                    <Producto key={producto.id}
+                    producto = {producto}/>
+                ))}
             </tbody>
 
         </table>
-    </Fragment>
+        {loading ? "cargando..." : null}
+        {error ? <div className="font-weight-bold alert alert-danger text-center mt-4">Hubo un error</div> : null }
+        </Fragment>
     );
 };
 
